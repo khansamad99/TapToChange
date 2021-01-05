b2b0e60d8ae80af71e3700c826dbe8f53b953c40
Samad Khan <samadk619@gmail.com>
2021-01-05 12:58:55 +0530

    First App

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
 
-export default function App() {
+const App = () => {
+
+  const [randomColor,setRandomColor] = useState('rgb(32,0,126)')
+
+  const changeBG = () => {
+
+    let color = "rgb(" + 
+              Math.floor(Math.random() * 256)+
+              ","+
+              Math.floor(Math.random() * 256)+
+              ","+
+              Math.floor(Math.random() * 256)+
+              ")"
+              
+    setRandomColor(color)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={[styles.container,{backgroundColor:randomColor}]}>
+        <TouchableOpacity onPress={changeBG}>
+          <Text style={styles.text}>Tap To Change</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
   },
+  text:{
+    fontSize: 30,
+    backgroundColor: "#BB2CD9",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    color: "#FFFFFF",
+    borderRadius: 15,
+    textTransform: "uppercase",
+  }
 });
+
+export default App;
